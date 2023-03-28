affc2dcb4d2f07804297bdcf6737188bb1ea3d9a
Arthur Marinheiro Vilela <arthurmarinheirovilela@gmail.com>
2023-03-27 21:57:01 -0300

    Added discord bot dependency

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	conf "github.com/ardanlabs/conf/v3"
+	"github.com/bwmarrin/discordgo"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"os"
@@ -11,6 +12,10 @@ import (
 
 type configuration struct {
 	Token string `conf:"help:'Token used to authenticate the bot.',required,noprint"`
+}
+
+func (c *configuration) getToken() string {
+	return fmt.Sprintf("Bot %s", c.Token)
 }
 
 func main() {
@@ -27,13 +32,33 @@ func main() {
 	log := zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).
 		With().Timestamp().Logger()
 
-	if err := run(&log); err != nil {
+	if err := run(&log, cfg); err != nil {
 		log.Fatal().Err(err).Msg("Fatal error.")
 	}
 }
 
-func run(log *zerolog.Logger) error {
+func run(log *zerolog.Logger, c *configuration) error {
 	log.Info().Msg("Starting up bot.")
+
+	log.Info().Msg("Creating session.")
+	session, err := discordgo.New(c.getToken())
+	if err != nil {
+		return errors.Wrap(err, "Unable to create session.")
+	}
+
+	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Info().Str("username", s.State.User.Username).Str("discriminator", s.State.User.Discriminator).Msg("Session logged in.")
+	})
+
+	log.Info().Msg("Starting session.")
+	err = session.Open()
+	if err != nil {
+		return errors.Wrap(err, "Unable to open session.")
+	}
+
+	log.Info().Msg("Closing session.")
+	session.Close()
+
 	log.Info().Msg("Shutting down bot.")
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.18
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.5
+	github.com/bwmarrin/discordgo v0.27.1
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.29.0
 )
 
 require (
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
