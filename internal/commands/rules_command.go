d3a4b5c7349f030b67aa6f717b62edc804a2de59
Arthur Marinheiro Vilela <arthurmarinheirovilela@gmail.com>
2023-03-28 21:46:33 -0300

    Added show rules command

--- internal/commands/rules_command.go
+++ internal/commands/rules_command.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/rs/zerolog"
 )
@@ -8,6 +9,11 @@ import (
 type RulesCommand struct {
 	log     *zerolog.Logger
 	command *discordgo.ApplicationCommand
+	Rules   []Rule
+}
+
+type Rule struct {
+	Text string
 }
 
 func NewRulesCommand(log *zerolog.Logger) *RulesCommand {
@@ -16,12 +22,32 @@ func NewRulesCommand(log *zerolog.Logger) *RulesCommand {
 		command: &discordgo.ApplicationCommand{
 			Name:        "rules",
 			Description: "Comando para gerenciar e consutar as regras do servidor.",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "show",
+					Description: "Mostra todas regras.",
+					Type:        discordgo.ApplicationCommandOptionSubCommand,
+				},
+			},
 		},
+		Rules: []Rule{Rule{Text: "Ado ado ado."}, Rule{Text: "Cado cado cado."}},
 	}
 }
 
 func (c *RulesCommand) Handle(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	options := i.ApplicationCommandData().Options
 
+	switch options[0].Name {
+	case "show":
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{
+					c.createRulesEmbed(),
+				},
+			},
+		})
+	}
 }
 
 func (c *RulesCommand) Command() *discordgo.ApplicationCommand {
@@ -30,4 +56,21 @@ func (c *RulesCommand) Command() *discordgo.ApplicationCommand {
 
 func (c *RulesCommand) CommandName() string {
 	return c.command.Name
+}
+
+func (c *RulesCommand) createRulesEmbed() *discordgo.MessageEmbed {
+	fields := make([]*discordgo.MessageEmbedField, len(c.Rules))
+
+	for i, rule := range c.Rules {
+		fields[i] = &discordgo.MessageEmbedField{
+			Name:  fmt.Sprintf("Regra N°%v", i+1),
+			Value: rule.Text,
+		}
+	}
+
+	return &discordgo.MessageEmbed{
+		Title:       "Regras",
+		Description: "Regras do servidor.",
+		Fields:      fields,
+	}
 }
